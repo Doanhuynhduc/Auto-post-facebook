b3abcec074e9eea49258c138dbb2c73999dfdbbf
huynhduc <huynhduc7447@gmail.com>
2020-05-30 14:33:18 +0700

    xay dung cay thu muc

---
+++ auto-post-facebook.php
@@ -0,0 +1,8 @@
+/*
+ Plugin Name: Auto post facebook hdd
+Description:   Tự động đăng bài viết lên facebook
+Version: 1.0
+Author: huynh duc dev
+Author URI: https://facebook/hd7447
+License: GPLv2 or later
+*/
